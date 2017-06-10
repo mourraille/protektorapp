a8fcf2995a4cadd3c05a5aba374041be5f8809c6
Mauricio Mourraille <mauricio.mourraille@ucrso.info>
2017-06-10 04:53:55 -0600

    commit inicial

---
+++ platforms/browser/www/js/app.js
@@ -0,0 +1,53 @@
+var app = {
+    initialize: function() {
+        this.bindEvents();
+    },
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    openNativeAppWindow: function(data) {
+        window.open(data, '_system');
+    }
+};
+
+document.addEventListener('deviceready',function() {
+
+
+    var lock = new Auth0Lock(
+      // All these properties are set in auth0-variables.js
+      AUTH0_CLIENT_ID,
+      AUTH0_DOMAIN
+    );
+
+    var userProfile;
+
+    $('.btn-login').click(function(e) {
+      e.preventDefault();
+      lock.show(function(err, profile, token) {
+        if (err) {
+          // Error callback
+          console.log("There was an error");
+          alert("There was an error logging in");
+        } else {
+          // Success calback
+
+          // Save the JWT token.
+          localStorage.setItem('userToken', token);
+
+          // Save the profile
+          userProfile = profile;
+
+          $('.login-box').hide();
+          $('.logged-in-box').show();
+          $('.nickname').text(profile.nickname);
+          $('.nickname').text(profile.name);
+          $('.avatar').attr('src', profile.picture);
+          $('.token').text(token);
+        }
+      });
+    });
+
+}, false);
---
+++ platforms/browser/www/js/auth0-variables.js
@@ -0,0 +1,3 @@
+var AUTH0_CLIENT_ID='xm9XIMKejv7Cq877Btro0nhgQSPJ7rjk';
+var AUTH0_CALLBACK_URL=location.href;
+var AUTH0_DOMAIN='mourraille.auth0.com';
